9e300745ccfceecf6befec4f6398c252f356897b
janechester <jane_chester@hotmail.com>
2016-01-17 23:00:41 -0800

    Set up site

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var http = require("http");
+var express = require( 'express' );
+var app = express();
+var http = require( 'http' ).Server( app );
 var port = process.env['PORT'] || 8080;
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    res.end('<!DOCTYPE html><html><meta charset="utf-8"><title>It works' +
-            "</title><b>It works!</b><br /><br />This is the server's " +
-            "default server.js.");
-}).listen(port);
-console.log("Server ready to accept requests on port %d", port);
+app.use( express.static( __dirname + '/' ) );
+
+app.get( '/', function( req, res ) {
+  res.sendFile( __dirname + '/janechesterportfolio/janechesterportfolio.html' );
+ } );
+
+http.listen( port, function() {
+  console.log( 'Listening for connections' );
+} );
